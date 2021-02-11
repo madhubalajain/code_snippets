8c0b4a6a85db12d369d42a42fe3c0e213f956264
madhubalajain <35684761+madhubalajain@users.noreply.github.com>
2021-02-11 16:12:09 +0530

    Update oop.py

--- Object-Oriented/2-Class-Instance-Variables/oop.py
+++ Object-Oriented/2-Class-Instance-Variables/oop.py
@@ -1,5 +1,8 @@
 
 class Employee:
+    
+    num_of_emp = 0
+    raise_amount = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -7,8 +10,23 @@ class Employee:
         self.email = first + '.' + last + '@email.com'
         self.pay = pay
 
+        Employee.num_of_emp += 1
+        
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self)
+        self.pay = int(self.pay * self.raise_amount)
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
+
+emp_1.raise_amount = 1.05 # This will create copy in the instance variable, because in apply_raise, this is reffered as self. instead of Employee. . This also helps in the Inheritance, we want to override the value of it
+
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+
+print(emp_1.__dict__)
+
+#https://www.youtube.com/watch?v=BJ-VvGyQxho
