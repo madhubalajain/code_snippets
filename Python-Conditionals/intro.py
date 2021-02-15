7022bea9bf02d22e7781357327b50187ea298706
madhubalajain <35684761+madhubalajain@users.noreply.github.com>
2021-02-15 14:18:28 +0530

    Update intro.py

--- Python-Conditionals/intro.py
+++ Python-Conditionals/intro.py
@@ -5,3 +5,36 @@ if condition:
     print('Evaluated to True')
 else:
     print('Evaluated to False')
+
+    # Object Identity:  is
+    This check where the object reference in memory is same or not
+    
+    #Python don't have switch statement, rather it has as if elif else to do that
+    
+    # Boolean operator
+    and
+    or
+    not
+    
+    
+    a = [1, 2, 3]
+    b = [1, 2, 3]
+    
+    if( a == b) # True
+        print('true')
+       
+    if( a is b) #False
+        print('False')
+        
+    a = [1, 2, 3]
+    b = a
+    
+    print(a is b) #True. print(id(a) == id(b))
+    print(a == b) #True
+    
+    # False Values:
+    # False
+    # None
+    # Zero of any numeric type
+    # Any empty sequence. For example, '', (), [].
+    # Any empty mapping. For example, {}.
