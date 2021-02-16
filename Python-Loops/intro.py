ec84dc2f580d1c66346051c004d8a62bd58cdee4
madhubalajain <35684761+madhubalajain@users.noreply.github.com>
2021-02-16 13:42:10 +0530

    Update intro.py

--- Python-Loops/intro.py
+++ Python-Loops/intro.py
@@ -6,3 +6,30 @@ while True:
     #     break
     print(x)
     x += 1
+    
+#Interating over list
+
+nums = [1, 2, 3, 4, 5]
+
+for num in nums
+    print(num)
+    
+# Break and continue loop
+
+for i in range(10)
+    print(i)
+    
+ #print i from 0 to 10
+
+for i in range(1,11)
+    print(i)
+    
+#print i from 1 to 10
+
+#while loop
+
+x = 0
+
+while x < 10:
+    print(x)
+    x += 1
