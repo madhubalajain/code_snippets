92594fd96105b9d514c659cc72b64fe47ef10158
madhubalajain <35684761+madhubalajain@users.noreply.github.com>
2021-02-25 14:19:08 +0530

    Update comprehensions.py

--- List_Comp/comprehensions.py
+++ List_Comp/comprehensions.py
@@ -6,6 +6,10 @@ for n in nums:
   my_list.append(n)
 print my_list
 
+#list comprehession
+
+my_list [ n for n in nums]
+
 print [n for n in nums]
 
 # I want 'n*n' for each 'n' in nums
@@ -14,9 +18,13 @@ print [n for n in nums]
 #   my_list.append(n*n)
 # print my_list
 
+my_list = [ n*n for n in nums]
+
 # Using a map + lambda
 # my_list = map(lambda n: n*n, nums)
 # print my_list
+
+# the above code can be replaced as with Line no 21
 
 # I want 'n' for each 'n' in nums if 'n' is even
 # my_list = []
@@ -25,9 +33,13 @@ print [n for n in nums]
 #     my_list.append(n)
 # print my_list
 
+my_list = [ n for n in nums if n%2 == 0]
+
 # Using a filter + lambda
 # my_list = filter(lambda n: n%2 == 0, nums)
 # print my_list
+
+# the above code can be replaced as with Line no 36
 
 # I want a (letter, num) pair for each letter in 'abcd' and each number in '0123'
 # my_list = []
@@ -35,6 +47,8 @@ print [n for n in nums]
 #   for num in range(4):
 #     my_list.append((letter,num))
 # print my_list
+
+my_list [(letter, num) for letter in 'abcd' for num in range(4)]
 
 # Dictionary Comprehensions
 names = ['Bruce', 'Clark', 'Peter', 'Logan', 'Wade']
@@ -47,9 +61,11 @@ heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
 #     my_dict[name] = hero
 # print my_dict
 
-
+my_dict = {name : hero for name , hero in zip(names,heros)}
 
 # If name not equal to Peter
+
+my_dict = {name : hero for name , hero in zip(names,heros) if name != 'Peter'}
 
 # Set Comprehensions
 # nums = [1,1,2,1,3,4,3,4,5,5,6,7,8,7,9,9]
@@ -58,6 +74,7 @@ heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
 #     my_set.add(n)
 # print my_set
 
+my_set = { n*n for n in nums}
 
 # Generator Expressions
 # I want to yield 'n*n' for each 'n' in nums
@@ -68,6 +85,8 @@ nums = [1,2,3,4,5,6,7,8,9,10]
 #         yield n*n
 
 # my_gen = gen_func(nums)
+
+my_gen = (n*n for n in nums) 
 
 # for i in my_gen:
 #     print i
